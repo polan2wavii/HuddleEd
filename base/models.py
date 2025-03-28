30c8f2a7249a5d83f9bc6d8e5b671ee7cfc9f888
Amedi13 <160151075+Amedi13@users.noreply.github.com>
2025-03-27 21:33:00 -0400

    models data and functions

--- base/models.py
+++ base/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#create models here
+
+# course of a study session 
+class Course(models.Model): 
+    name = models.CharField(max_length=300); 
+
+    def __str__(self):
+        return self.name
+
+#study session mdoel
+class Study_Session(models.Model):
+    #unqiue attributes of a study rsession
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(null=True, blank=True)
+    preferences = models.TextChoices(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    #order based on creation
+    class Meta: 
+        ordering = ['-updated', 'created']
+    
+    def __str__(self): 
+        return self.name
+
--- base/views.py
+++ base/views.py
@@ -1,12 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django. contrib.auth.forms import UserCreationForm
+from django.db.models import Q 
+from django.contrib import messages
+from .models import Course, Study_Session
 
 # Create your views here.
 def home(request):
+    #render all study sessions 
     return render(request, 'home.html')
 
-def room(request):
-    return render(request, 'room.html')
+def session(request, pk):
+    session = Study_Session.objects.get(id=pk)
+    description = session.description
+    course = session.course
+    preferences = session.preferences
+
+    context = {'session': session, 'desciprtion': description, 'course': course, 'prefrences': preferences}
+
+    return render(request, 'base/session.html', context)
 
 def profile(request):
     return render(request, 'profile.html')
+
+def register(request):
+    account = UserCreationForm()
+    if request.method == 'POST': 
+        account = UserCreationForm(request.POST)
+    if account.is_valid(): 
+        user = account.save(commit=False)
+        user.username = user.username.lower()
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else: 
+        messages.error("An error has occured. Try again later")
+    return render(request, 'base/register.html')
+
+def loginPage(request): 
+    page: 'login'
+    user = authenticate(username=request.POST.get('username'), passowrd=request.POST.get('password')) 
+
+    if user is not None and user.is_authenticated:
+       login(request, user)
+       return redirect('home')
+    else: 
+        messages.error("username or password does not exist")
+
+    return render(request, 'base/login_page.html')
+
+def logutUser(request):
+    logout(request) 
+    return redirect('base/login_page.html')
+
+def create_session():
+    return 1
+
+def update_sessoin(): 
+    return 1
